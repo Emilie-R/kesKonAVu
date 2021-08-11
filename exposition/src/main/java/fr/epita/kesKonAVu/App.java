5cec3d404b3ea8cb662891f5f90582595ebdf045
DESKTOP-1E81GG5\stagiaire2 <klebert.jean1louis@gmail.com>
2021-08-11 10:01:52 +0200

    suppression @ComponentScan

--- exposition/src/main/java/fr/epita/kesKonAVu/App.java
+++ exposition/src/main/java/fr/epita/kesKonAVu/App.java
@@ -2,10 +2,8 @@ package fr.epita.kesKonAVu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "fr.epita.kesKonAVu" })
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
